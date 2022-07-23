b6881ab908d3922da8848e61b615c48dbf9a257a
aLeuleu <aurelien.levra@gmail.com>
2022-07-23 14:57:04 +0200

    update

--- ex00/includes/rush02.h
+++ ex00/includes/rush02.h
@@ -1,18 +1,14 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   main.c                                             :+:      :+:    :+:   */
+/*   rush02.h                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: alevra <alevra@student.42lyon.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2022/07/23 10:50:21 by alevra            #+#    #+#             */
-/*   Updated: 2022/07/23 12:27:12 by alevra           ###   ########lyon.fr   */
+/*   Created: 2022/07/23 11:04:39 by alevra            #+#    #+#             */
+/*   Updated: 2022/07/23 14:57:01 by alevra           ###   ########lyon.fr   */
 /*                                                                            */
 /* ************************************************************************** */
-
-#include"rush02.h"
-
-
 
 typedef struct s_element_dict
 {
@@ -20,23 +16,14 @@ typedef struct s_element_dict
 	char	*value;
 } t_element_dict;
 
+int		how_many_splits(char *input);
 
+t_element_dict	*split_by_element(char *input, int size);
 
-int main(int argc, char **argvalue)
-{
-	
-	int a;
-	a =0;
+void	fill_tab_element(t_element_dict	*tab_element);
 
-	t_element_dict element;
+char	*fetch_element(char *key);
 
-	element.key = "0";
-	element.value = "zero";
+void	display_tab_element(t_element_dict	*tab_element);
 
-	t_element_dict element2;
-
-	element2.key = "1";
-	element2.value = "one";
-
-	return 0;
-}
+t_element_dict	*parse_dict()
--- myriam/main.c
+++ myriam/main.c
@@ -5,38 +5,13 @@
 /*                                                    +:+ +:+         +:+     */
 /*   By: alevra <alevra@student.42lyon.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2022/07/23 10:50:21 by alevra            #+#    #+#             */
-/*   Updated: 2022/07/23 12:27:12 by alevra           ###   ########lyon.fr   */
+/*   Created: 2022/07/23 12:37:17 by alevra            #+#    #+#             */
+/*   Updated: 2022/07/23 12:39:45 by alevra           ###   ########lyon.fr   */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include"rush02.h"
-
-
-
-typedef struct s_element_dict
+int main(int argc, char **argv)
 {
-	char	*key;
-	char	*value;
-} t_element_dict;
-
-
-
-int main(int argc, char **argvalue)
-{
-	
-	int a;
-	a =0;
-
-	t_element_dict element;
-
-	element.key = "0";
-	element.value = "zero";
-
-	t_element_dict element2;
-
-	element2.key = "1";
-	element2.value = "one";
-
+	/* code */
 	return 0;
 }
