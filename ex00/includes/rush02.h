aff69a317fcb38262febc1e43f0bc727e3ac8229
aLeuleu <aurelien.levra@gmail.com>
2022-07-23 16:58:15 +0200

    update

--- ex00/includes/rush02.h
+++ ex00/includes/rush02.h
@@ -6,7 +6,7 @@
 /*   By: alevra <alevra@student.42lyon.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/07/23 11:04:39 by alevra            #+#    #+#             */
-/*   Updated: 2022/07/23 14:57:01 by alevra           ###   ########lyon.fr   */
+/*   Updated: 2022/07/23 16:58:04 by alevra           ###   ########lyon.fr   */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,14 +16,12 @@ typedef struct s_element_dict
 	char	*value;
 } t_element_dict;
 
+t_element_dict	*parse_dict();
+
 int		how_many_splits(char *input);
 
-t_element_dict	*split_by_element(char *input, int size);
+void	set_keys(t_element_dict	*tab_element);
 
-void	fill_tab_element(t_element_dict	*tab_element);
-
-char	*fetch_element(char *key);
+char	*fetch_values(char *key);
 
 void	display_tab_element(t_element_dict	*tab_element);
-
-t_element_dict	*parse_dict()
